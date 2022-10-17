bcec6c1cffcc9b69dc631ca6a1ac652fbdab07ec
Vanh020104 <114908216+Vanh020104@users.noreply.github.com>
2022-10-17 14:39:18 +0700

    Create bai1_lap8.cpp

---
+++ bai1_lap8.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+	int a, b;
+	printf("\nNhap a: ");
+	scanf("%d", &a);
+	printf("\nNhap b:");
+	scanf("%d", &b);
+
+     	int temp = a;//Luu gia tri cho a
+	   a = b;
+	  b = temp;// lay gia tri cua a cho temp b;
+	  printf("a = %d, b = %d", a, b);
+	  int arr[a]; 
+	  printf("/nDia chi cua a: %d", &arr);
+	  
+	  int dch[b]; 
+	  printf("\nDia chi cua b: %d", &dch);
+	return 0;
+} 
+
+ 
