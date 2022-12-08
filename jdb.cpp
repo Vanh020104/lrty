0b0d5d6db986175c706dc142c54248d74da55394
Vanh020104 <114908216+Vanh020104@users.noreply.github.com>
2022-12-08 12:50:51 +0700

    Create jdb.cpp

---
+++ jdb.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void swapFunc(*x,int *y) {
+	//swapping... 
+}
+voi bubbleSort(int arr[], int n){
+	//sorting here... 
+} 
+void insertionSort(int arr[], int n){
+	
+} 
+//In ket qua... 
+void printResult(int arr[], int n){
+	//printing the result... 
+} 
+int main (){
+	int arr[]={12,23,33,55,25};
+    int n = sizeof(arr)/sizeof(arr[0]); 
+	bubbleSort(arr,n);
+	printf("\nMang sau khi duoc sap xep: ");
+	printfResult(arr,n); 
+	return 0; 
+} 
