d8b8e3e40284c123778606dd79c6df0e701c445f
Vanh020104 <114908216+Vanh020104@users.noreply.github.com>
2022-12-08 12:53:24 +0700

    Create dia chi Mang 2.cpp

---
+++ dia chi Mang 2.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int a[5];
+	int arr[3];
+	int *ptr;
+	printf("\nDia chi cua Mang la: %d", &a[5]);
+		printf("\nDia chi cua Mang la: %d", &arr[3]);
+	
+	return 0;
+}
