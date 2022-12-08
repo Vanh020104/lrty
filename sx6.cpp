de5acc6cd3e61035b8d8dc439f1376e2389b7128
Vanh020104 <114908216+Vanh020104@users.noreply.github.com>
2022-12-08 12:53:50 +0700

    Create sx6.cpp

---
+++ sx6.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+	int i,j,n,temp;
+	int a[100];
+	printf("\nSo phan tu cua Mang: ",n);
+	scanf("%d", &n);
+	for(i=0;i<n;i++){
+		printf("\nPhan tu thu: ",i+1);
+		scanf("%d", &a[i]); 
+	} 
+	for(i=0;i<n-1;i++) 
+	for(j=0;j<n-i-1;j++){
+		if(a[j]>a[j+1]){
+			int temp = a[j];
+			a[j]=a[j+1];
+			a[j+1]=temp; 
+		} 
+	} 
+	printf("\nMang duoc sap xep la: ");
+	for(i=0;i<n;i++)
+	printf("%d",a[i]);
+	return 0; 
+} 
