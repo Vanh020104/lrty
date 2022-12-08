08eefdf4cd0e1fb4c32d7872ebec152f2210cb3a
Vanh020104 <114908216+Vanh020104@users.noreply.github.com>
2022-12-08 12:53:17 +0700

    Create Cap phat them.cpp

---
+++ Cap phat them.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdio.h>
+//Realloc: cap phat bo sung ( neu thieu) 
+int main (){
+	int n, i;
+	n = 5;
+	ptr = (int*)c	int *ptr;
+alloc(n, sizeof(int));
+	//if check here
+	printf("\nPhan tu Mang: ");
+	for(i = 0; i < n; i++){
+		printf("\n %d", ptr[i]);
+	}
+	
+	
+	
+	
+	
+	return 0;
+} 
+re
+	printf("\nPhan tu Mang: ");
+	for(i = 0; i < n; i++){
+		printf("\n %d", ptr[i]);
+	}
